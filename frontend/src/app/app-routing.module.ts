99d496638f0b6f03f2af3249c5dcde75fa559f9f
leidypaez <leidy16paez@gmail.com>
2021-11-18 18:52:38 -0500

    se sube la primera parte del frotntend iseños de logo para navegador y celular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:LoginComponent},
+  {path: "login", component:LoginComponent},
+  {path: "register", component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { LogoComponent } from './component/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
     LoginComponent,
     RegisterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
