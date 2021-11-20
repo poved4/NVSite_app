86d16feb243dc5406a18a37ae18280355c6f1f1f
LuisMarcano1812 <luismarcano1812@gmail.com>
2021-11-20 09:42:47 -0500

    login and register without functionalities

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { registerLocaleData } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {path: "", component:LoginComponent},
+  {path: "login", component:LoginComponent},
+  {path: "register", component:RegisterComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
